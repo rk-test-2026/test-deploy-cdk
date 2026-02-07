b5c1e085d6351f8ed0f3dc2e9510801e674f311a
Rajesh <rajeshbtech09@gmail.com>
2026-02-07 17:39:39 -0600

    IAM

--- iac/src/iam.ts
+++ iac/src/iam.ts
@@ -2,6 +2,7 @@
 import { Construct } from "constructs";
 import { IamRole } from "../.gen/providers/aws/iam-role";
 import { IamRolePolicyAttachment } from "../.gen/providers/aws/iam-role-policy-attachment";
+import { IamServiceLinkedRole } from "./.gen/providers/aws/iam-service-linked-role";
 
 export class IamStack {
   executionRole: IamRole;
@@ -24,6 +25,11 @@ export class IamStack {
       role: this.executionRole.name,
       policyArn:
         "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+    });
+
+
+    new IamServiceLinkedRole(this, "ecs_service_linked_role", {
+      awsServiceName: "ecs.amazonaws.com",
     });
 
     this.taskRole = new IamRole(scope, "task-role", {
