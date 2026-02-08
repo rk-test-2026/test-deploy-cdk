9bb16479bc664d013aaa76ac48e0d5dbe3f83c24
Rajesh <rajeshbtech09@gmail.com>
2026-02-07 20:42:03 -0600

    publish image now

--- iac/src/network.ts
+++ iac/src/network.ts
@@ -3,6 +3,9 @@ import { Construct } from "constructs";
 import { Vpc } from "../.gen/providers/aws/vpc";
 import { Subnet } from "../.gen/providers/aws/subnet";
 import { SecurityGroup } from "../.gen/providers/aws/security-group";
+import { InternetGateway } from "../.gen/providers/aws/internet-gateway";
+import { RouteTable } from "../.gen/providers/aws/route-table";
+import { RouteTableAssociation } from "../.gen/providers/aws/route-table-association";
 
 export class NetworkStack {
   vpc: Vpc;
@@ -13,6 +16,22 @@ export class NetworkStack {
     this.vpc = new Vpc(scope, "vpc", {
       cidrBlock: "10.0.0.0/16",
       tags: { Name: `${cfg.env}-vpc` }
+    });
+
+    const igw = new InternetGateway(this, "igw", {
+      vpcId: this.vpc.id,
+      tags: { Name: `${cfg.env}-igw` }
+    });
+
+    const publicRouteTable = new RouteTable(this, "public-rt", {
+      vpcId: this.vpc.id,
+      route: [
+        {
+          cidrBlock: "0.0.0.0/0",
+          gatewayId: igw.id,
+        }
+      ],
+      tags: { Name: `${cfg.env}-public-rt` }
     });
 
     this.subnets = ["a", "b"].map((az, i) =>
@@ -42,6 +61,13 @@ export class NetworkStack {
           cidrBlocks: ["0.0.0.0/0"]
         }
       ]
+    });
+
+    this.subnets.forEach((subnet, index) => {
+      new RouteTableAssociation(this, `rt-assoc-${index}`, {
+        subnetId: subnet.id,
+        routeTableId: publicRouteTable.id
+      });
     });
   }
 }
