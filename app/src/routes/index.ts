004dd5e97281e64b664a22f606d81d75ab8d7cf8
Rajesh <rajeshbtech09@gmail.com>
2026-02-08 00:06:30 -0600

    add extra endpoint

--- app/src/routes/index.ts
+++ app/src/routes/index.ts
@@ -10,4 +10,9 @@ router.get('/health', (_req, res) => {
   res.send('App is Healthy');
 });
 
+router.get('/info', (_req, res) => {
+  res.send('This is info endpoint ');
+});
+
+
 export default router;
